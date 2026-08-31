17d6c296eb3bb0bfc36ca065642763cd87c3d2ba
1U25CS012-ATCHAYA <atchayaatchaya588@gmail.com>
2026-08-31 11:36:17 +0530

    Update answers.sql

--- starter/answers.sql
+++ starter/answers.sql
@@ -1,1 +1,23 @@
+drop database if exists college;
 
+create database college;
+use college;
+
+CREATE TABLE Student (
+    StudentID INT PRIMARY KEY,
+    StudentName VARCHAR(20) NOT NULL UNIQUE,
+    DOB DATE NOT NULL,
+    Gender VARCHAR(10) NOT NULL,
+    DepartmentID INT NOT NULL
+);
+desc Student;
+
+insert Student values("101","ATCHAYA","2007-11-05","Female","1001");
+select * from Student;
+
+alter table Student add Email varchar(30);
+alter table Student add Phonenumber integer(10);
+desc Student;
+insert Student values("102", "ATCHAYA","2006-11-15", "Female","10002", "atchayaatchaya588@gmail.com", "9876543210");
+desc Student;
+select * from Student;
