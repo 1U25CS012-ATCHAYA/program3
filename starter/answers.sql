e02579b59a2a7242b6356748d273ae070bc8633e
1U25CS012-ATCHAYA <atchayaatchaya588@gmail.com>
2026-08-31 11:40:37 +0530

    Update answers.sql

--- starter/answers.sql
+++ starter/answers.sql
@@ -12,12 +12,12 @@ CREATE TABLE Student (
 );
 desc Student;
 
-insert Student values("101","ATCHAYA","2007-11-05","Female","1001");
+insert Student values("101","ATCHAYA","2007-11-15","Female","1001");
 select * from Student;
 
 alter table Student add Email varchar(30);
 alter table Student add Phonenumber integer(10);
 desc Student;
-insert Student values("102", "ATCHAYA","2006-11-15", "Female","10002", "atchayaatchaya588@gmail.com", "9876543210");
+insert Student values("101", "ATCHAYA","2006-11-15", "Female","10002", "atchayaatchaya588@gmail.com", "9876543210");
 desc Student;
 select * from Student;
